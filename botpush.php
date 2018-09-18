26398b77a6971588c83558ef9b67daee716fe978
ThisIsVegas <thisisvegas007@gmail.com>
2018-09-18 11:52:46 +0700

    Update access token, push ID and channel secret

--- botpush.php
+++ botpush.php
@@ -4,11 +4,11 @@
 
 require "vendor/autoload.php";
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'ULtkIAg2GBViAgFTMFmjyp0cGbiGKWf+qFhivuDsxvS+P+hliI+CK516jpFrnlJd3hu9vf7QVsuoZZyoyVflGpThMuFFFoqHc3hR5TAeQ3olCRXZkaIwkTRAVknrNNC1WMG9kLufsOYfrt45BfnEHwdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
+$channelSecret = 'de41f619ef1c4910c2726b82a35179d0';
 
-$pushID = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
+$pushID = 'Ud77c725ab37e75a515e81696af07d2ed';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
--- profile.php
+++ profile.php
@@ -1,9 +1,9 @@
 <?php
 
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'ULtkIAg2GBViAgFTMFmjyp0cGbiGKWf+qFhivuDsxvS+P+hliI+CK516jpFrnlJd3hu9vf7QVsuoZZyoyVflGpThMuFFFoqHc3hR5TAeQ3olCRXZkaIwkTRAVknrNNC1WMG9kLufsOYfrt45BfnEHwdB04t89/1O/w1cDnyilFU=';
 
-$userId = 'Uffa138efe037e6e889d0b0f4a871c005';
+$userId = 'Ud77c725ab37e75a515e81696af07d2ed';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
