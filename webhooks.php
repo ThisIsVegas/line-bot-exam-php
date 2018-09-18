4b406ac240845030bd4ef606b949180914774fea
Veerakran Sereerungruangkul <thisisvegas007@gmail.com>
2018-09-18 10:59:14 +0700

    Change access token

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'xrV9EagiiDzqU5HBOhn6TZRmhNrsnAkDI/cgpywVILbCNFGoMxamzktQNchDX9RE3hu9vf7QVsuoZZyoyVflGpThMuFFFoqHc3hR5TAeQ3pZ4WAdj2LBRQwSQLuBkYLpNh67DjLybPTLQW1ZZq5fcQdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
